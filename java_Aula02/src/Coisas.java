9f657b957b80ee3e9c0446b8264403cb57092d61
unknown <a46505@HP-OE4-546255.esferreiradias.local>
2015-04-29 08:59:41 +0100

    init commit

---
+++ java_Aula02/src/Coisas.java
@@ -0,0 +1,40 @@
+
+public class Coisas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int    i = 19;
+		float  f = 23.2f;
+		double d = 111.23232d;
+		
+		double d2 = d + f;
+		
+		int a = 0,
+			b = 9,
+			c = 8;
+		
+		char p = 'p';
+		
+		boolean h = true;
+		boolean y = (1 == 0);
+		
+		String s = "OLA";
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		System.out.println("ola " + s);
+
+	}
+
+}
